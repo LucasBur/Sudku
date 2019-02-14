f6f333ba2eaabec1ce2aea315214a5acf4e51c2f
Unknown <lucas.burette@edu.itescia.fr>
2019-02-14 10:48:57 +0100

    Implementation SudokuGrid

--- src/SudokuGrid.php
+++ src/SudokuGrid.php
@@ -3,4 +3,69 @@
 class SudokuGrid implements GridInterface
 {
     /* Insérer le code ici */
+    public static function loadFromFile($var): ?SudokuGrid
+    {
+        $file = file_get_contents($var);
+        if ($file == null) {
+            return null;
+        }
+        else {
+            return $file;
+        }
+    }
+
+    public function __construct(array $data)
+    {
+        return null;
+    }
+
+    public function get(int $rowIndex, int $columnIndex): int
+    {
+        return null;
+    }
+
+    public function set(int $rowIndex, int $columnIndex, int $value): void
+    {
+
+    }
+
+    public function row(int $rowIndex): array
+    {
+        return null;
+    }
+
+    public function column(int $columnIndex): array
+    {
+        return null;
+    }
+
+    public function square(int $squareIndex): array
+    {
+        return null;
+    }
+
+    public function display(): string
+    {
+        return null;
+    }
+
+    public function isValueValidForPosition(int $rowIndex, int $columnIndex, int $value): bool
+    {
+        return true;
+    }
+
+    public function getNextRowColumn(int $rowIndex, int $columnIndex): array
+    {
+        return null;
+    }
+
+    public function isValid(): bool
+    {
+        return true;
+    }
+
+    public function isFilled(): bool
+    {
+        return true;
+    }
 }
